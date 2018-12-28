339af1a3933baf9a52f6690580c7c6bf531294ff
iiMohamedYT88 <46201203+iiMohamedYT88@users.noreply.github.com>
2018-12-28 22:02:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,8 +25,8 @@ client.on("message", (message) => {
     .setTitle(`:mailbox_with_mail: Vulnix Help`)
     .setColor(0xCF40FA)
     .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    .addField(`Tickets`, `[${prefix}new](*) > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[${prefix}help](*) > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
     message.channel.send({ embed: embed });
   }
 
@@ -86,4 +86,4 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 });
 
-client.login(token);
+client.login(NTI4MzAwNzY5MjYzMzUzODY2.DwgSDQ.S1-fClcDzha1f3SEoIGB-f7_YwE);
